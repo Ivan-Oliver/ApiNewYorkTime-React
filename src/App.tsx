560aa866b5f215612e05bd9e5f1ed24131bf9595
Ivan-Oliver <ivanoliverfunes15@gmail.com>
2023-02-14 17:47:40 +0100

    previa a los formularios

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,14 @@
-import { useState, useEffect } from 'react';
-import { booklist, fetchData, getListDetails } from "../src/services/api"
-import Card from './components/card/card';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
-import { MaxContenedor, MainContainer, ContainerDetails, Title, ButtonAmazon, EnlaceAmazon, Image, Description, BackButton } from './components/card/style'
+import Router from '../src/container/Router'
 
 function App() {
-  const [data, setData] = useState<booklist[]>([])
-  const [dataDetails, setDataDetails] = useState<any[]>([])
-  const [showList, setShowList] = useState<boolean>(true)
-  useEffect(() => {
-    fetchData().then(data => {
-      if (data) {
-        setData(data)
-      }
-    })
-  }, [])
-
-  const callListDetails = async (listName: string) => {
-    const data = await getListDetails(listName)
-    setDataDetails(data)
-    setShowList(false)
-  }
-
   return (
-    <ThemeProvider theme={theme}>
-        <MaxContenedor>
-          {showList && data?.map(bookList => (
-            <Card 
-            title={bookList.display_name} 
-            description={bookList.updated} 
-            newest_published_date={bookList.newest_published_date} 
-            oldest_published_date={bookList.oldest_published_date} 
-            listName={bookList.list_name_encoded} 
-            onClick={callListDetails}
-            />
-          ))}
-          <header><BackButton className="back" onClick={() => setShowList(true)}>Back</BackButton></header>
-          {!showList && dataDetails?.map(listDetails => (
-            <>
-            <MainContainer>
-            <ContainerDetails>
-                <Title>{listDetails.title}</Title>
-               <Image src={listDetails.book_image} ></Image>
-                <Description>{listDetails.description}</Description>
-                <ButtonAmazon><EnlaceAmazon href={listDetails.amazon_product_url}>Amazon</EnlaceAmazon></ButtonAmazon>
-              </ContainerDetails>
-            </MainContainer>
-             </>
-            
-
-          ))}
-        </MaxContenedor>
-    </ThemeProvider>
-  );
+<ThemeProvider theme={theme}>
+    <Router/>
+  </ThemeProvider>
+  )
+  
 }
 
-export default App;
+export default App;
---
+++ src/views/Home/index.tsx
@@ -0,0 +1,61 @@
+import React, { FC } from 'react'
+import { useState, useEffect } from 'react'
+import { booklist, fetchData, getListDetails } from '../../services/api'
+import { ThemeProvider } from 'styled-components'
+import { MaxContenedor, MainContainer, ContainerDetails, Title, ButtonAmazon, EnlaceAmazon, Image, Description, BackButton } from '../../components/card/style'
+import theme from '../../styles/theme'
+import Card from '../../components/card/card'
+
+const Home: FC = () => {
+
+    const [data, setData] = useState<booklist[]>([])
+    const [dataDetails, setDataDetails] = useState<any[]>([])
+    const [showList, setShowList] = useState<boolean>(true)
+    useEffect(() => {
+        fetchData().then(data => {
+            if (data) {
+                setData(data)
+            }
+        })
+    }, [])
+
+    const callListDetails = async (listName: string) => {
+        const data = await getListDetails(listName)
+        setDataDetails(data)
+        setShowList(false)
+    }
+
+    return (
+        <ThemeProvider theme={theme}>
+            <MaxContenedor>
+                {showList && data?.map(bookList => (
+                    <Card
+                        title={bookList.display_name}
+                        description={bookList.updated}
+                        newest_published_date={bookList.newest_published_date}
+                        oldest_published_date={bookList.oldest_published_date}
+                        listName={bookList.list_name_encoded}
+                        onClick={callListDetails}
+                    />
+                ))}
+                <header><BackButton className="back" onClick={() => setShowList(true)}>Back</BackButton></header>
+                {!showList && dataDetails?.map(listDetails => (
+                    <>
+                        <MainContainer>
+                            <ContainerDetails>
+                                <Title>{listDetails.title}</Title>
+                                <Image src={listDetails.book_image} ></Image>
+                                <Description>{listDetails.description}</Description>
+                                <ButtonAmazon><EnlaceAmazon href={listDetails.amazon_product_url}>Amazon</EnlaceAmazon></ButtonAmazon>
+                            </ContainerDetails>
+                        </MainContainer>
+                    </>
+
+
+                ))}
+            </MaxContenedor>
+        </ThemeProvider>
+    )
+}
+
+export default Home
